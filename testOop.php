19d98c218cb09d69916e26380c005a5f6bc24eb0
luthfy13 <luthfy13@gmail.com>
2019-10-16 10:09:51 +0800

    latihan serkom

--- testOop.php
+++ testOop.php
@@ -1,8 +1,13 @@
 <?php
 	class Mahasiswa{
 		protected $nim;
+		private $name;
 
 		public function setNim($nim){
+			$this->nim = $nim;
+		}
+
+		public function setNama($nim){
 			$this->nim = $nim;
 		}
 
@@ -11,12 +16,22 @@
 	class MahasiswaTeknik extends Mahasiswa{
 		public function getNimTeknik(){
 			return $this->nim;
-			
+		}
+		public function getNamaTeknik(){
+			return $this->nama;
 		}
 	}
 
 
 	$mhs = new MahasiswaTeknik();
+	
+	//aman karena variabel $nim access modifier-nya protected
 	$mhs->setNim("001");
 	echo $mhs->getNimTeknik();
+
+	//error karena variabel $nim access modifier-nya private
+	//pesan error: "Notice: Undefined property: MahasiswaTeknik::$nama"
+	$mhs->setNama("002");
+	echo $mhs->getNamaTeknik();
+
 ?>
